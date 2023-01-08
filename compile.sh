330c07d4e55fafb9983e7ca7f34fadacd05fcfdc
Jan Krecke <jan.krecke@pm.me>
2023-01-08 11:00:14 +0900

    created compile script with chatgpt

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# The first argument passed to the script is the path to the cpp file
+cpp_file=$1
+
+# Extract the directory and filename from the cpp file path
+dir=$(dirname "$cpp_file")
+filename=$(basename "$cpp_file")
+
+# Use clang++ to compile the cpp file and store the executable in the target directory
+clang++ "$cpp_file" -o "target/$dir/${filename%.*}"
