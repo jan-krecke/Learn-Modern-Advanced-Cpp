362ddea076f4c4a2f96d0bf47a8151eee0b1ebe6
Jan Krecke <jan.krecke@pm.me>
2023-01-08 19:04:06 +0900

    member variable assignment to modern way

--- assignments/01_classes_and_strings/url.cpp
+++ assignments/01_classes_and_strings/url.cpp
@@ -1,6 +1,6 @@
 #include "url.hpp"
 
 URL::URL(const std::string& protocol, const std::string& resource)
-    : protocol_(protocol), resource_(resource) {}
+    : protocol_{protocol}, resource_{resource} {}
 
 std::string URL::display() { return protocol_ + "://" + resource_; }
